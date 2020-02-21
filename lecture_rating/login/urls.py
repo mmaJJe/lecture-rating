66ef85a9548571bc53936ab83fd41a339b675985
mmajje <bluekmky@likelion.org>
2020-02-21 17:05:11 +0900

    login.ver2

---
+++ lecture_rating/login/urls.py
@@ -0,0 +1,11 @@
+"""
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('',views.login ,name="login"),
+    path('signup/',views.signup ,name="signup"),
+]
---
+++ lecture_rating/login/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.contrib.auth import (authenticate, login as django_login, logout as django_logout)
+from django.contrib.auth.models import User
+from .forms import UserForm
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            django_login(request, user)
+            log = 1
+            request.session['username'] = username
+            request.session['log'] = log
+            return render(request, "login.html", {'log' : log})
+        else:
+            error = "다시 시도해주세요!"
+            return render(request, "login.html", { "error" : error})
+    else:
+        return render(request, "login.html")
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if form.is_valid() and cpassword == password:
+            new_user = User.objects.create_user(**form.cleaned_data)
+            django_login(request, new_user)
+            log = 1
+            request.session['username'] = username
+            request.session['log'] = log
+            return render(request, "login.html", {'log' : log})
+        else:
+            context = {"msg" : "회원가입 실패"}
+            return render(request, "signup.html", context)
+    else:
+        form = UserForm()
+        return render(request, "signup.html")
